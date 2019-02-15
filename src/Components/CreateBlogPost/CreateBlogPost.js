c75aaef5abbc9a2ebf5d011b5ba69edc5689c7c7
Hemant <hhsriteja@gmail.com>
2019-02-15 11:56:19 +0530

    changed form data to axios post

--- src/Components/CreateBlogPost/CreateBlogPost.js
+++ src/Components/CreateBlogPost/CreateBlogPost.js
@@ -6,47 +6,67 @@ import Footer from '../Footer/Footer';
 import axios from 'axios';
 
 class CreateBlogForm extends Component {
+  state = {
+    date: {},
+    title: '',
+    readTime: '',
+    message: '',
+  }
 
   sendPOSTReqData = (event) => {
+    let bodyFormData = new FormData();
     if (this.refs.titleInput !== '') {
       event.preventDefault();
-      let payload = {
-        date: this.refs.dateInput,
-        readTime: this.refs.readTimeInput,
-        title: this.refs.title,
-      }
-      axios.post('https://putsreq.com/fzMHu0JbMj7I1Hfmt1m9', payload);
+      bodyFormData.set('date', this.state.date);
+      bodyFormData.set('readTime', this.state.readTime);
+      bodyFormData.set('title', this.state.title);
+      bodyFormData.set('message', this.state.message);
+      axios.post('https://putsreq.com/3ooGv5XTGNDmZN5fiNCC', bodyFormData);
     }
+  }
+
+  handleDateChange(e){
+    this.setState({date: e.target.value})
+  }
+  handleTitleChange(e){
+    this.setState({title: e.target.value})
+  }
+
+  handleReadTimeChange(e){
+    this.setState({readTime: e.target.value})
+  }
+
+  handleMessageChange(e){
+    this.setState({message: e.target.value})
   }
 
   render() {
     return (
       <div className="body">
         <Navigation />
-        <form className="form-width" method='POST' action='https://putsreq.com/fzMHu0JbMj7I1Hfmt1m9'>
+        <form className="form-width" onSubmit={this.sendPOSTReqData}>
           <ul className="flex-outer">
             <li>
               <label >Date</label>
-              <input ref="dateInput" type="date" id="date" />
+              <input value={this.state.date} onChange={(e) => this.handleDateChange(e)} name="dateInput" type="date" id="date" />
             </li> 
             <li>
               <label >Read Time</label>
-              <input name="readTimeInput" type="text" id="read-time" placeholder="Enter the read time for blog" />
+              <input value={this.state.readTime} onChange={(e) => this.handleReadTimeChange(e)} type="text" name="read-time" placeholder="Enter the read time for blog" />
             </li>
             <li>
               <label >Title</label>
-              <input name="titleInput" type="text" id="title" placeholder="Enter the Title of Post" />
+              <input value={this.state.title} onChange={(e) => this.handleTitleChange(e)} type="text" name="title" placeholder="Enter the Title of Post" />
             </li>
             <li>
               <label >Message</label>
-              <textarea name="messageInput" rows="6" id="message" placeholder="Enter your message here"></textarea>
+              <textarea value={this.state.message} onChange={(e) => this.handleMessageChange(e)} rows="6" name="message" placeholder="Enter your message here"></textarea>
             </li>
             <li>
-
             <Link to="/">
               <button>Back</button>
             </Link>
-            <button type="Submit">Submit</button>
+            <button type='submit'>Submit</button>
             </li>
           </ul>
         </form>
